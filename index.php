1cd5ceed1d08ece881a3683cf960c0eb6583b6aa
Francisco Alcaraz <fran.alcaraz@live.com>
2018-09-19 10:02:57 -0300

    conec
    conexion y consultas basicas de sql

--- index.php
+++ index.php
@@ -113,23 +113,32 @@ include ('config/mysql.php');
           </div>
 
           <div class="row">
+            <?php
+              $sql = "SELECT * FROM articulos";
+            $art = $con->query($sql);
+              foreach($art as $row){
+              
+              ?>
 
             <div class="col-lg-4 col-md-6 mb-4">
               <div class="card h-100">
                 <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
                 <div class="card-body">
                   <h4 class="card-title">
-                    <a href="#">Item One</a>
+                    <a href="#"><?=$row['nombre_articulo']?></a>
                   </h4>
-                  <h5>$24.99</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                  <h5><?=$row['precio'];?></h5>
+                  <p class="card-text"><?=$row['articulo_descripcion'];?></p>
                 </div>
                 <div class="card-footer">
                   <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
                 </div>
               </div>
             </div>
-
+<?php
+              
+              }
+              ?>
             <div class="col-lg-4 col-md-6 mb-4">
               <div class="card h-100">
                 <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
