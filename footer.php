2d7cdf6b92463d2337bd4865f4ed481716bd8e0f
chinonrg <38039740+chinonrg@users.noreply.github.com>
2018-09-21 20:25:24 -0300

    Che, aca subo mi parte
    chupelupe

---
+++ footer.php
@@ -0,0 +1,50 @@
+<style>
+    footer{
+    color: white;
+    }
+    a{
+    color: white;
+    text-decoration: none;
+    }
+    a:hover{
+        color: white;
+        text-decoration: underline;
+    }
+</style>
+   <footer class="page-footer font-small bg-primary mx-auto">
+    <div class="container-fluid text-center text-md-left">
+      <div class="row">
+        <div class="col-4 text-center mx-auto">
+          <h5>DVapor</h5>
+          <p>eCommerce que no persigue fines de lucro, unicamente propositos educativos (?)</p>
+          </div>
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+        <div class="col-8 mx-auto">
+           
+            <h5>Nuestras Marcas</h5>
+
+            <ul class="list-unstyled">
+              
+                <a href="http://www.vaporesso.com"><img src="img/vaporeso_logo.png" alt="vaporesso logo"></a>
+           
+                <a href="http://www.smok.com"><img src="img/smok_logo.png" alt="smok logo"></a>
+             
+             
+                <a href="http://www.elementeliquids.com"><img src="img/element_logo.png" alt="element logo"></a>
+              
+             
+                <a href="http://www.charlieschalkdust.com"><img src="img/ccd_logo.png" alt="ccd logo"></a>
+              
+            </ul>
+          </div>
+          </div>
+
+    </div>
+
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="https://davinci.edu.ar">Escuela de Arte Multimedial Da Vinci</a>
+    </div>
+
+
+  </footer>
---
+++ header.php
@@ -0,0 +1,25 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">DVapor</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php?seccion=home">Home</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php?seccion=productos">Productos</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php?seccion=contacto">Contacto</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Buscar">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+    </form>
+  </div>
+</nav>
